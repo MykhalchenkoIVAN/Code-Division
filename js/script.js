a3d144561c4065060f087e2df6e40fa1b81d6a3f
Ivan <ivanogamer2019@gmail.com>
2022-10-04 09:56:48 +0300

    remove space

--- js/script.js
+++ js/script.js
@@ -1,22 +1,7 @@
 'use script'
-
-const headerLngBtn = document.querySelector('.header_lng_btn');
-const headerLng = document.querySelectorAll('.lng_txt');
-const headerLngContent = document.querySelector('.header_lng_content');
-// const inputEmail = document.querySelector('.input_email').value;
-// console.log(inputEmail, typeof inputEmail);
-
 const signInButton = document.getElementById('sign_in_button');
 
 
-
-
-headerLngContent.classList.add('display_none');
-
-
-headerLngBtn.onclick = function () {
-    headerLngContent.classList.remove('display_none');
-}
 
 signInButton.onclick = function () {
     const inputEmail = document.querySelector('.input_email').value;
@@ -25,11 +10,3 @@ signInButton.onclick = function () {
     console.log(inputPassword);
 }
 
-
-for (let i = 0; i < headerLng.length; i++) {
-    headerLng[i].addEventListener('click', function () {
-        headerLngBtn.innerHTML = headerLng[i].textContent;
-        headerLngContent.classList.add('display_none');
-        console.log(headerLng[i].textContent);
-    });
-}
